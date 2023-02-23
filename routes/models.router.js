e78e623c77bdf857142c361781bc43c0d8b0e2ec
Your Name <you@example.com>
2023-02-23 01:23:26 -0400

    without erros only miss put docs

--- routes/models.router.js
+++ routes/models.router.js
@@ -4,13 +4,14 @@ const routesUsers = require("./users.routes");
 // const isAuthenticatedByPassportJwt = require('../libs/passport')
 
 const routesAuth = require("./auth.routes");
+const authMiddleware = require("../middlewares/auth.middleware");
 
 function routerModels(app) {
   const router = express.Router();
 
-  app.use("/api/v1", router);
-  router.use("/auth", routesAuth);
-  router.use("/users", routesUsers);
+  app.use("/api/v1", authMiddleware, router);
+  router.use("/auth", authMiddleware, routesAuth);
+  router.use("/users", authMiddleware, routesUsers);
 }
 
 module.exports = routerModels;
