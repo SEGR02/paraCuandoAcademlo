50165e1192fc87d31e483d65e2baee77bc049192
eliomaure2022 <elio_maure@yahoo.com.ar>
2023-02-25 03:07:02 -0300

    add routes

--- routes/models.router.js
+++ routes/models.router.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const routesUsers = require("./users.routes");
 const routesTags = require("./tags.routes");
-
+const routesCountries = require("./countries.routes");
+const routesStates = require("./states.routes");
+const routesCities = require("./cities.routes");
+const routesRoles = require("./roles.routes");
 // const isAuthenticatedByPassportJwt = require('../libs/passport')
 
 const routesAuth = require("./auth.routes");
@@ -13,6 +16,10 @@ function routerModels(app) {
   router.use("/auth", routesAuth);
   router.use("/users", routesUsers);
   router.use("/tags", routesTags);
+  router.use("/countries", routesCountries);
+  router.use("/states", routesStates);
+  router.use("/cities", routesCities);
+  router.use("/roles", routesRoles);
 }
 
 module.exports = routerModels;
